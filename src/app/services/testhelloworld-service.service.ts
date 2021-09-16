a7b3c4d91431135eafc117cf5576f78543dffa65
sivarr143 <siva.pericherla@dataken.net>
2021-09-16 19:34:57 +0530

    successfull code

--- src/app/services/testhelloworld-service.service.ts
+++ src/app/services/testhelloworld-service.service.ts
@@ -10,11 +10,11 @@ export class TesthelloworldServiceService {
   constructor(private http : HttpClient, private sel : SelectedValuesService) { }
 
   getdata(){
-    if(this.sel.menuoptions === 'helloWorld'){
-    return this.http.get<any>("http://localhost:7777/"+this.sel.menuoptions,{responseType: 'text' as 'json'});
-    }
-    else{
+    // if(this.sel.menuoptions === 'helloWorld'){
+    // return this.http.get<any>("http://localhost:7777/"+this.sel.menuoptions,{responseType: 'text' as 'json'});
+    // }
+    // else{
       return this.http.get<any>("http://localhost:7777/matches/"+this.sel.menuoptions+"/"+this.sel.season, {responseType: 'text' as 'json'});
-    }
+    // }
   }
 }
